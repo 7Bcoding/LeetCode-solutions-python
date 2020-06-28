9b704ff913b47ffb81c9392df8fab06f69ce3c25
7Bcoding <541965325@qq.com>
2020-06-28 22:15:23 +0800

    20200628，买卖股票的最佳时机

--- 双指针遍历/q122-MaxProfit-买卖股票的最佳时机2-easy.py
+++ 双指针遍历/q122-MaxProfit-买卖股票的最佳时机2-easy.py
@@ -7,6 +7,6 @@ class Solution(object):
         profit = 0
         n = len(prices)
         for i in range(1, n):
-            if prices[i] > prices[i-1]:
-                profit += (prices[i] - prices[i-1])
+            if prices[i] > prices[i - 1]:
+                profit += (prices[i] - prices[i - 1])
         return profit
