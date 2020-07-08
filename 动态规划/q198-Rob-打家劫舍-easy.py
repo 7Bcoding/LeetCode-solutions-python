2d24d10165c0d732d8fadc79a2cb4c552ed28a71
7Bcoding <541965325@qq.com>
2020-07-08 23:30:24 +0800

    20200708,动态规划相关

---
+++ 动态规划/q198-Rob-打家劫舍-easy.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:return 0
+        if n == 1:return nums[0]
+        if n == 2:return max(nums[0], nums[1])
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1] - nums[i - 1] + nums[i], dp[i - 2] + nums[i])
+        return max(dp[-1], dp[-2])
