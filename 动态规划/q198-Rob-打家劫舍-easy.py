6d4c9d931010b60cfe35eaacc954f8448974ef9f
7Bcoding <541965325@qq.com>
2020-07-09 10:03:27 +0800

    20200709更新，动态规划

--- 动态规划/q198-Rob-打家劫舍-easy.py
+++ 动态规划/q198-Rob-打家劫舍-easy.py
@@ -12,5 +12,5 @@ class Solution(object):
         dp[0] = nums[0]
         dp[1] = nums[1]
         for i in range(2, n):
-            dp[i] = max(dp[i - 1] - nums[i - 1] + nums[i], dp[i - 2] + nums[i])
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
         return max(dp[-1], dp[-2])
