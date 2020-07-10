cf2a135ea0c69ecb1d5d1a454adc86ec8f7ea559
7Bcoding <541965325@qq.com>
2020-07-10 23:51:39 +0800

    20200710,动态规划、状态机

---
+++ 动态规划/q309-MaxProfit-买卖股票的最佳时机含冷冻期-mid.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        输入: [1,2,3,0,2]
+        输出: 3
+        解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+        """
+        if not prices:
+            return 0
+        n = len(prices)
+        # f[i][0]: 手上持有股票的最大收益
+        # f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+        # f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+        f = [[-prices[0], 0, 0]] + [[0] * 3 for _ in range(n - 1)]
+        for i in range(1, n):
+            f[i][0] = max(f[i - 1][0], f[i - 1][2] - prices[i])
+            f[i][1] = f[i - 1][0] + prices[i]
+            f[i][2] = max(f[i - 1][1], f[i - 1][2])
+
+        return max(f[n - 1][1], f[n - 1][2])
