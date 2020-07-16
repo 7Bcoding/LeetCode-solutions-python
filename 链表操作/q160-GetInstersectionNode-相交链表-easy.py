87ed0c9e698e05fc888698d21565d62dbf9ced59
7Bcoding <541965325@qq.com>
2020-07-16 21:27:46 +0800

    20200716更新,链表

---
+++ 链表操作/q160-GetInstersectionNode-相交链表-easy.py
@@ -0,0 +1,18 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        cur1 = headA
+        cur2 = headB
+        while cur1 != cur2:
+            cur1 = cur1.next if cur1 else headB
+            cur2 = cur2.next if cur2 else headA
+        return cur1
