ea9dadfe1b85a6a8e70b078d61775df39d994437
7Bcoding <541965325@qq.com>
2020-08-03 22:40:02 +0800

    20200803更新,二叉树相关

--- 树相关/二叉树/q1367-IsSubPath-二叉树中的列表-mid.py
+++ 树相关/二叉树/q1367-IsSubPath-二叉树中的列表-mid.py
@@ -1,66 +1,54 @@
-
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-class TreeNode(object):
-    def __init__(self, x, left, right):
-        self.val = x
-        self.left = left
-        self.right = right
-
-
 class Solution(object):
-    def isSubPath(self, head, root):
-        """
-        :type head: ListNode
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if root is None or head is None: return False
-        nodelist = []
+    class Solution(object):
+        def isSubPath(self, head, root):
+            """
+            :type head: ListNode
+            :type root: TreeNode
+            :rtype: bool
+            """
+            # 个人解法：递归，DFS，记忆化搜索
 
-        def dfs_Treelist(root, head, listnode, nodelist):
-            if listnode is None: return True
-            if root is None and listnode:
+            def dfs_Treelist(root, head, listnode):
+                if listnode is None: return True
+                if root is None and listnode:
+                    return False
+                if root.val != listnode.val and listnode == head:
+                    return dfs_Treelist(root.left, head, listnode) or \
+                           dfs_Treelist(root.right, head, listnode)
+                elif root.val != listnode.val and listnode != head:
+                    return False
+                elif root.val == listnode.val and listnode == head:
+                    if dfs_Treelist(root.left, head, listnode.next) or \
+                            dfs_Treelist(root.right, head, listnode.next):
+                        return True
+                    if dfs_Treelist(root.left, head, listnode) or \
+                           dfs_Treelist(root.right, head, listnode):
+                        return True
+                return dfs_Treelist(root.left, head, listnode.next) or \
+                       dfs_Treelist(root.right, head, listnode.next)
+
+            return dfs_Treelist(root, head, head)
+
+        # 官方解法
+
+        def isSubPath(self, head, root):
+            """
+            :type head: ListNode
+            :type root: TreeNode
+            :rtype: bool
+            """
+            if not head:
+                return True
+            if not root:
                 return False
-            if root.val != listnode.val and listnode == head:
-                flag = dfs_Treelist(root.left, head, listnode, nodelist)
-                flag = dfs_Treelist(root.right, head, listnode, nodelist)
-            elif root.val != listnode.val and listnode != head:
+            if root.val != head.val:
                 return False
-            else:
-                nodelist.append(root.val)
-                flag = dfs_Treelist(root.left, head, listnode.next, nodelist)
-                if flag: return True
-                if nodelist: nodelist.pop()
-                flag = dfs_Treelist(root.right, head, listnode.next, nodelist)
-                if flag: return True
-                if nodelist: nodelist.pop()
-            return flag
 
-        flag = dfs_Treelist(root.right, head, head, nodelist)
-        print(nodelist)
-        return flag
+            return self.dfs(head.next, root.left) or self.dfs(head.next, root.right)
 
-
-if __name__ == '__main__':
-    solution = Solution()
-    root = TreeNode(1, TreeNode(4, None, TreeNode(2, TreeNode(1, None, None), None)),
-                    TreeNode(4, TreeNode(2, TreeNode(6, None, None), TreeNode(8, TreeNode(1, None, None), TreeNode(3, None, None))), None))
-    list = [1,4,2,6,8]
-    node = ListNode(0)
-    dummy = ListNode(0)
-    dummy.next = node
-    for i in range(len(list)):
-        node.val = list[i]
-        node.next = ListNode(0)
-        node = node.next
-    node = dummy.next
-    for i in range(len(list)):
-        print(node.val, end='')
-        node = node.next
-    res = solution.isSubPath(dummy.next, root)
-    print(res)
+        def isSubPath(self, head, root):
+            if not root:
+                return False
+            if self.dfs(head, root):
+                return True
+            return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
