bd76f6b8df70cbcee0dfca0e424147daf88f7b5d
7Bcoding <541965325@qq.com>
2020-08-04 19:15:51 +0800

    20200804更新,动态规划相关

--- 动态规划/q300-LengthOfLIS-最长上升子序列-mid.py
+++ 动态规划/q300-LengthOfLIS-最长上升子序列-mid.py
@@ -4,33 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) ==0: return 0
-        n, maxnum, max_l = len(nums), -1, 0
-        dp = [1] * n
-
-        for k in range(1, n):
-            for i in range(k, n):
-                if nums[i] <= nums[i - 1]:
-                    dp[i] = dp[i - 1]
-                else:
-                    if nums[i] > maxnum:
-
-                        maxnum = nums[i]
-                        print(maxnum, end=' ')
-                        dp[i] = dp[i-1] + 1
+    # 单调栈 + 二分查找
+        d = []
+        for n in nums:
+            if not d or n > d[-1]:
+                d.append(n)
+            else:
+                l, r = 0, len(d) - 1
+                loc = r
+                while l <= r:
+                    mid = (l + r) // 2
+                    if d[mid] >= n:
+                        loc = mid
+                        r = mid - 1
                     else:
-                        dp[i] = dp[i - 1]
+                        l = mid + 1
+                d[loc] = n
+        return len(d)
 
-                max_l = max(max_l, dp[i])
-
-        # print(dp)
-        return max_l if max_l else 1
-
-
-if __name__ == '__main__':
-    solution = Solution()
-    print(solution.lengthOfLIS([10,9,2,5,3,4,5,5,1,5,6,7,101,6,19]))
-    # print(solution.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
-    # # print(solution.lengthOfLIS([2, 2, 2, 3, 1]))
-    # print(solution.lengthOfLIS([2, 2, 2]))
-    # print(solution.lengthOfLIS([1, 3, 6, 7, 9, 4, 10, 5, 6]))
+    # 动态规划解法
+    def lengthOfLIS(self, nums):
+        if not nums:
+            return 0
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
