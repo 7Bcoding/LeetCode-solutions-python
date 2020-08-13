bc77070fea8ed01fa0238403d06df2d141fe2b96
7Bcoding <541965325@qq.com>
2020-08-13 23:05:58 +0800

    20200813更新，二叉树序列化

--- 树相关/二叉树/q297-BinaryTreeSerialize-二叉树的序列化与反序列化-hard.py
+++ 树相关/二叉树/q297-BinaryTreeSerialize-二叉树的序列化与反序列化-hard.py
@@ -1,9 +1,9 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Codec:
 
@@ -13,6 +13,23 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        if not root: return "[]"
+        queue = [root]
+        res_tree = []
+        while queue:
+            root = queue.pop(0)
+            if root:
+                res_tree.append(str(root.val))
+                queue.append(root.left)
+                queue.append(root.right)
+            else:
+                res_tree.append('null')
+        for i in range(len(res_tree) - 1, -1, -1):
+            if res_tree[i] is 'null':
+                res_tree.pop(i)
+            else:
+                break
+        return '[' + ','.join(res_tree) + ']'
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -20,7 +37,18 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        if data == '[]':
+            return None
+        nodeval = data[1:-1].split(',')
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
+        def dfs(i):
+            if i > len(nodeval) - 1:
+                return None
+            if nodeval[i] != 'null':
+                root = TreeNode(int(nodeval[i]))
+                root.left = dfs(2 * i + 1)
+                root.right = dfs(2 * i + 2)
+            else:
+                root = None
+            return root
+        return dfs(0)
