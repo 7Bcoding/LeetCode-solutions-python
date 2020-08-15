f7def6d2257d27a8a70bec96bf6790dca8356c3b
7Bcoding <541965325@qq.com>
2020-08-15 12:40:28 +0800

    20200815更新，动态规划

--- 动态规划/q96-NumTrees-不同的二叉搜索树-mid.py
+++ 动态规划/q96-NumTrees-不同的二叉搜索树-mid.py
@@ -15,15 +15,15 @@ class Solution(object):
         #  每次遍历，将经过的每个节点的dp值相加得到sum，即为当前节点的dp，直至末尾，到达最后一个
         #  节点时，dp[n]的值即为所求
         if n == 0: return 0
-        dp = [1] * (n+1)
-        for i in range(1, n+1):
+        dp = [1] * (n + 1)
+        for i in range(1, n + 1):
             sum = 0
-            for j in range(1, i+1):
+            for j in range(1, i + 1):
                 if j == 1:
-                    sum += dp[i-j]
+                    sum += dp[i - j]
                 elif j == i:
-                    sum += dp[j-1]
+                    sum += dp[j - 1]
                 else:
-                    sum += (dp[i-j] * dp[j-1])
+                    sum += (dp[i - j] * dp[j - 1])
             dp[j] = sum
         return dp[n]
