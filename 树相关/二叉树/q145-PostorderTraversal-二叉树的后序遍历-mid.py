3f39daa2cf35512c8779d7a017da45ef4032d045
10015826 <541965325@qq.com>
2020-08-30 20:12:31 +0800

    20200830更新，二叉树相关

---
+++ 树相关/二叉树/q145-PostorderTraversal-二叉树的后序遍历-mid.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # 解法：迭代
+        # 从根节点开始依次迭代，弹出栈顶元素输出到输出列表中，然后依次压入它的所有孩子节点，
+        # 按照从上到下、从左至右的顺序依次压入栈中。
+        # 因为深度优先搜索后序遍历的顺序是从下到上、从左至右，所以需要将输出列表逆序输出。
+
+        if root is None:
+            return []
+
+        stack, output = [root, ], []
+        while stack:
+            root = stack.pop()
+            output.append(root.val)
+            if root.left is not None:
+                stack.append(root.left)
+            if root.right is not None:
+                stack.append(root.right)
+
+        return output[::-1]
