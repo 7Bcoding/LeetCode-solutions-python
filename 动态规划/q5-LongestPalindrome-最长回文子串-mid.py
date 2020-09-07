c04de7c249e69108a2ec643d465df129b1662760
cqyfrank <541965325@qq.com>
2020-09-07 18:20:43 +0800

    20200907更新，动态规划相关

--- 动态规划/q5-LongestPalindrome-最长回文子串-mid.py
+++ 动态规划/q5-LongestPalindrome-最长回文子串-mid.py
@@ -1,5 +1,16 @@
 class Solution:
     def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # 解法：动态规划
+        # dp[i][j] 表示 s 中 i 到 j 是否为回文串
+        # 1. i == j，单个字符，是回文子串，dp[i][j]=True
+        # 2. j-i == 1 ，两个字符，在s[i] == s[j]的情况下s[i][j]为回文子串，dp[i][j=True
+        # 3. j-i > 1，dp[i+1][j-1]为True (即s[i+1][j-1]为回文子串的情况下)，
+        # 同时满足s[i] == s[j]，即s[i][j]为回文子串，dp[i][j]=True
+        # 当 dp[i][j] = True 时，更新最大回文子串长度
         maxl = 0
         n = len(s)
         dp = [[False] * n for _ in range(n)]
--- 动态规划/q647-CountSubStrings-回文子串-mid.py
+++ 动态规划/q647-CountSubStrings-回文子串-mid.py
@@ -8,7 +8,8 @@ class Solution(object):
         # dp[i][j] 表示 s 中 i 到 j 是否为回文串
         # 1. i == j，单个字符，是回文子串，dp[i][j]=True
         # 2. j-i == 1 ，两个字符，在s[i] == s[j]的情况下s[i][j]为回文子串，dp[i][j]=True
-        # 3. j - i > 1，dp[i+1][j-1]为True (即s[i+1][j-1]为回文子串的情况下)，同时满足s[i] == s[j]，即s[i][j]为回文子串，dp[i][j]=True
+        # 3. j-i > 1，dp[i+1][j-1]为True (即s[i+1][j-1]为回文子串的情况下)，
+        # 同时满足s[i] == s[j]，即s[i][j]为回文子串，dp[i][j]=True
         count = 0
         n = len(s)
         dp = [[False] * n for _ in range(n)]
