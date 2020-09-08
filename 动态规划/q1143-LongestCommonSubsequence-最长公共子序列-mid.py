2dbd9f44f21c356d68c4ac757a781f2e3f9b5978
7Bcoding <541965325@qq.com>
2020-09-08 13:28:45 +0800

    20200908更新，动态规划

--- 动态规划/q1143-LongestCommonSubsequence-最长公共子序列-mid.py
+++ 动态规划/q1143-LongestCommonSubsequence-最长公共子序列-mid.py
@@ -29,7 +29,7 @@ class Solution(object):
             for j in range(1, n + 1):
                 if text1[i - 1] == text2[j - 1]:
                     # 找到一个 lcs 中的字符
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
         return dp[-1][-1]
