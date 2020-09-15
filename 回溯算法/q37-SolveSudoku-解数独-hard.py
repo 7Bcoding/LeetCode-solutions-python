57e876ac42e00da21029cc605f02df8f75b0f5df
7Bcoding <541965325@qq.com>
2020-09-15 13:41:14 +0800

    20200915更新，回溯

--- 回溯算法/q37-SolveSudoku-解数独-hard.py
+++ 回溯算法/q37-SolveSudoku-解数独-hard.py
@@ -7,89 +7,36 @@ class Solution:
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
+        # 解法：递归回溯
 
-        def could_place(d, row, col):
-            """
-            Check if one could place a number d in (row, col) cell
-            """
-            return not (d in rows[row] or d in columns[col] or \
-                        d in boxes[box_index(row, col)])
+        def dfs(pos):
+            nonlocal valid
+            if pos == len(spaces):
+                valid = True
+                return
 
-        def place_number(d, row, col):
-            """
-            Place a number d in (row, col) cell
-            """
-            rows[row][d] += 1
-            columns[col][d] += 1
-            boxes[box_index(row, col)][d] += 1
-            board[row][col] = str(d)
+            i, j = spaces[pos]
+            for digit in range(9):
+                if line[i][digit] == column[j][digit] == block[i // 3][j // 3][digit] is False:
+                    line[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
+                    board[i][j] = str(digit + 1)
+                    dfs(pos + 1)
+                    line[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = False
+                if valid:
+                    return
 
-        def remove_number(d, row, col):
-            """
-            Remove a number which didn't lead
-            to a solution
-            """
-            del rows[row][d]
-            del columns[col][d]
-            del boxes[box_index(row, col)][d]
-            board[row][col] = '.'
+        line = [[False] * 9 for _ in range(9)]
+        column = [[False] * 9 for _ in range(9)]
+        block = [[[False] * 9 for _a in range(3)] for _b in range(3)]
+        valid = False
+        spaces = list()
 
-        def place_next_numbers(row, col):
-            """
-            Call backtrack function in recursion
-            to continue to place numbers
-            till the moment we have a solution
-            """
-            # if we're in the last cell
-            # that means we have the solution
-            if col == N - 1 and row == N - 1:
-                nonlocal sudoku_solved
-                sudoku_solved = True
-            # if not yet
-            else:
-                # if we're in the end of the row
-                # go to the next row
-                if col == N - 1:
-                    backtrack(row + 1, 0)
-                # go to the next column
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    spaces.append((i, j))
                 else:
-                    backtrack(row, col + 1)
+                    digit = int(board[i][j]) - 1
+                    line[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
 
-        def backtrack(row=0, col=0):
-            """
-            Backtracking
-            """
-            # if the cell is empty
-            if board[row][col] == '.':
-                # iterate over all numbers from 1 to 9
-                for d in range(1, 10):
-                    if could_place(d, row, col):
-                        place_number(d, row, col)
-                        place_next_numbers(row, col)
-                        # if sudoku is solved, there is no need to backtrack
-                        # since the single unique solution is promised
-                        if not sudoku_solved:
-                            remove_number(d, row, col)
-            else:
-                place_next_numbers(row, col)
-
-        # box size
-        n = 3
-        # row size
-        N = n * n
-        # lambda function to compute box index
-        box_index = lambda row, col: (row // n) * n + col // n
-
-        # init rows, columns and boxes
-        rows = [defaultdict(int) for i in range(N)]
-        columns = [defaultdict(int) for i in range(N)]
-        boxes = [defaultdict(int) for i in range(N)]
-        for i in range(N):
-            for j in range(N):
-                if board[i][j] != '.':
-                    d = int(board[i][j])
-                    place_number(d, i, j)
-
-        sudoku_solved = False
-        backtrack()
-
+        dfs(0)
