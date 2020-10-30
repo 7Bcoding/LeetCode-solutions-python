d98b52f1dd2fe7ec4a536951d1baf52922025af3
cenquanyu <cenquanyu@baidu.com>
2020-10-30 21:50:42 +0800

    20201030更新，二叉树

--- 树相关/二叉树/q129-SumNumbers-求根到叶子节点数字之和-mid.py
+++ 树相关/二叉树/q129-SumNumbers-求根到叶子节点数字之和-mid.py
@@ -13,20 +13,20 @@ class Solution(object):
         """
         # 解法：回溯算法
         res = []
-        numarr = []
+        arr = []
 
         def sumtree(root):
             if not root:
-                numarr.append('')
+                arr.append('')
                 return 0
-            numarr.append(str(root.val))
+            arr.append(str(root.val))
             if not root.left and not root.right:
-                res.append(int(''.join(numarr[:])))
+                res.append(int(''.join(arr[:])))
                 return
             sumtree(root.left)
-            numarr.pop()
+            arr.pop()
             sumtree(root.right)
-            numarr.pop()
+            arr.pop()
             return False
         sumtree(root)
         return 0 if len(res) == 0 else sum(res)
