12cb402261735b9d3fa44e99e0b6b326b9393cf3
cenquanyu <cenquanyu@baidu.com>
2022-05-26 12:28:29 +0800

    20220526更新，回文链表

---
+++ 链表操作/q234-IsPalindrome-回文链表-easy.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None or head.next is None:
+            return True
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return vals == vals[::-1]
