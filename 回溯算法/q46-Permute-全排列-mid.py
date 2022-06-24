d77bd5ac9c00a8e3c080ba8978fa4824ed0d4504
cenquanyu <cenquanyu@baidu.com>
2022-06-24 12:45:52 +0800

    20220624更新，全排列

--- 回溯算法/q46-Permute-全排列-mid.py
+++ 回溯算法/q46-Permute-全排列-mid.py
@@ -4,25 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        memory = []
-        n = len(nums)
-        res = []
-        if len(nums) == 0: return []
         # 解法：回溯算法
         # 用一个记忆数组memory，深度遍历时，记录已经添加的元素，到递归的下一层，
         # 从头遍历nums时，就跳过memory中的元素，添加不在记忆数组里的元素。
         # 递归边界条件：当记忆数组长度达到nums时，加入res结果集，然后返回上一层
         # 递归返回时，弹出之前一层添加的元素
+        result = []
+        array = []
 
-        def dfspermute(memory, res):
-            if len(memory) == n:
-                res.append(memory[:])
+        def backtrack():
+            if len(array) == len(nums):
+                result.append(array[:])
                 return
-            for i in range(n):
-                if nums[i] not in memory:
-                    memory.append(nums[i])
-                    dfspermute(memory, res)
-                    memory.pop()
-        dfspermute(memory, res)
-        return res
+            for i in range(len(nums)):
+                if nums[i] not in array:
+                    array.append(nums[i])
+                    backtrack()
+                    array.pop()
+
+        backtrack()
+        return result
 
