9d5cebfeaa5ff2ee7f81b62905885804870d878f
cenquanyu <cenquanyu@baidu.com>
2022-08-30 09:55:32 +0800

    20220830更新，滑动窗口-无重复字符最长子串

--- 滑动窗口/q3-LengthOfLongestSubstring-无重复字符的最长子串-mid.py
+++ 滑动窗口/q3-LengthOfLongestSubstring-无重复字符的最长子串-mid.py
@@ -6,12 +6,12 @@ class Solution(object):
         """
         repset = set()
         n = len(s)
-        p, ans = -1, 0
+        p, max_len = -1, 0
         for i in range(n):
             if i != 0:
                 repset.remove(s[i - 1])
             while p+1 < n and s[p + 1] not in repset:
                 repset.add(s[p + 1])
                 p += 1
-            ans = max(ans, p - i + 1)
-        return ans
+            max_len = max(max_len, p - i + 1)
+        return max_len
